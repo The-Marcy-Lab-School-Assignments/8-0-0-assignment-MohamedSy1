f9547c594aefbeed4362bff7aa7a2796eddc069e
MohamedSy1 <mohamed.sy@compscihigh.org>
2024-04-02 12:37:41 -0400

    done

---
+++ first-server/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+
+const serveIndex = (req, res, next) => {
+  res.sendFile(__dirname + "/index.html");
+};
+
+const serveAbout = (req, res, next) => {
+  res.send("<h1>About</h1>");
+};
+
+const serveHello = (req, res, next) => {
+    const name = req.query.name || "world";
+    res.send(`<h1>Hello ${name}</h1>`);
+}
+
+const serveFood = (req, res, next) => {
+    const food = [{ food: 'spaghetti', food: 'steak'}]
+    res.send(food)
+}
+
+const serveFriends = (req, res, next) => {
+    const friends = [{ name: 'Bauze', name: 'El' }]
+    res.send(friends)
+}
+
+app.get('/', serveIndex);
+app.get('/about', serveAbout);
+app.get('/hello', serveHello);
+app.get('/api/food', serveFood);
+app.get('/api/friends', serveFriends);
+
+const port = 8080;
+app.listen(port, () => console.log(`listening at http://localhost:${port}`));
