055e2eba4fd51f78dc203ef58191b6c2a93a94a1
MohamedSy1 <mohamed.sy@compscihigh.org>
2024-04-06 14:34:13 -0400

    temp

--- first-server/index.js
+++ first-server/index.js
@@ -5,30 +5,21 @@ const serveIndex = (req, res, next) => {
   res.sendFile(__dirname + "/index.html");
 };
 
-const serveAbout = (req, res, next) => {
-  res.send("<h1>About</h1>");
+const serveData = (req, res, next) => {
+  const data = [{ Batman: "Bat mobile" }, { SuperMan: "fly" }];
+  res.send(data);
 };
 
-const serveHello = (req, res, next) => {
-    const name = req.query.name || "world";
-    res.send(`<h1>Hello ${name}</h1>`);
-}
+const dynamicServe = (req, res, next) => {
+  const name = req.query.name || "stranger";
+  res.send(`Hello ${name}`);
+};
 
-const serveFood = (req, res, next) => {
-    const food = [{ food: 'spaghetti', food: 'steak'}]
-    res.send(food)
-}
+app.get("/", serveIndex);
+app.get("/hello", dynamicServe);
+app.get("/api/data", serveData);
 
-const serveFriends = (req, res, next) => {
-    const friends = [{ name: 'Bauze', name: 'El' }]
-    res.send(friends)
-}
-
-app.get('/', serveIndex);
-app.get('/about', serveAbout);
-app.get('/hello', serveHello);
-app.get('/api/food', serveFood);
-app.get('/api/friends', serveFriends);
-
-const port = 8080;
-app.listen(port, () => console.log(`listening at http://localhost:${port}`));
+const PORT = 5500;
+app.listen(PORT, () => {
+  `Hello listening to port: ${PORT}`;
+});
